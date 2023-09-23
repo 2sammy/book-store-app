7c03dfe2a1d916b631f3adcd0096ab562d7df4c8
2sammy <samuelmanoah61@gmail.com>
2023-09-23 10:12:23 +0300

    not working

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
  const PORT = 5555;
+ const mongoDBURL = 'mongodb+srv://samuelmanoah:Say6102@app-store.no7isqe.mongodb.net/?retryWrites=true&w=majority'
 
- module.exports = PORT
+ module.exports = {PORT, mongoDBURL}
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const PORT = require("./config");
+const mongoDBURL = require("./config");
+const mongoose= require('mongoose');
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send("welcom to Mern Stack")
 });
+
+mongoose.connect(mongoDBURL)
+.then(()=>{
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`)
+    });
+})
 
